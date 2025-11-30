6dfa77129e7ed2cb9907e31b658c829a76b12aab
ben <koppe.development@gmail.com>
2025-11-30 10:34:41 -0500

    Autoformat .lua files

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,82 +5,82 @@ vim.g.mapleader = " "
 -- mnw is a global set by mnw
 -- so if it's set this config is being ran from nix
 if mnw ~= nil then
-	local current_file = debug.getinfo(1, "S").source:sub(2)
-	local two_dirs_up = vim.fn.fnamemodify(current_file, ":p:h:h:h")
-	vim.g.lazyvim_json = two_dirs_up .. "/lazyvim.json"
-	require("lazy").setup({
-		dev = {
-			path = mnw.configDir .. "/pack/mnw/opt",
-			-- match all plugins
-			patterns = { "" },
-			-- fallback to downloading plugins from git
-			-- disable this to force only using nix plugins
-			fallback = true,
-		},
+  local current_file = debug.getinfo(1, "S").source:sub(2)
+  local two_dirs_up = vim.fn.fnamemodify(current_file, ":p:h:h:h")
+  vim.g.lazyvim_json = two_dirs_up .. "/lazyvim.json"
+  require("lazy").setup({
+    dev = {
+      path = mnw.configDir .. "/pack/mnw/opt",
+      -- match all plugins
+      patterns = { "" },
+      -- fallback to downloading plugins from git
+      -- disable this to force only using nix plugins
+      fallback = true,
+    },
 
-		-- keep rtp/packpath the same
-		performance = {
-			reset_packpath = false,
-			rtp = {
-				reset = false,
-			},
-		},
+    -- keep rtp/packpath the same
+    performance = {
+      reset_packpath = false,
+      rtp = {
+        reset = false,
+      },
+    },
 
-		install = {
-			-- install the missing plugins
-			missing = true,
-			colorscheme = { "tokyonight", "habamax" },
-		},
+    install = {
+      -- install the missing plugins
+      missing = true,
+      colorscheme = { "tokyonight", "habamax" },
+    },
 
-		spec = {
-			-- add LazyVim and import its plugins
-			{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-			-- disable mason.nvim while using nix
-			-- precompiled binaries do not agree with nixos, and we can just make nix install it for us
-			{ "mason-org/mason-lspconfig.nvim", enabled = false },
-			{ "mason-org/mason.nvim", enabled = false },
-			-- import/override with your plugins
-			{ import = "plugins" },
-		},
-	})
+    spec = {
+      -- add LazyVim and import its plugins
+      { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+      -- disable mason.nvim while using nix
+      -- precompiled binaries do not agree with nixos, and we can just make nix install it for us
+      { "mason-org/mason-lspconfig.nvim", enabled = false },
+      { "mason-org/mason.nvim", enabled = false },
+      -- import/override with your plugins
+      { import = "plugins" },
+    },
+  })
 else
-	-- otherwise we have to bootstrap lazy ourselves
-	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-	if not (vim.uv or vim.loop).fs_stat(lazypath) then
-		local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-		local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-		if vim.v.shell_error ~= 0 then
-			vim.api.nvim_echo({
-				{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-				{ out, "WarningMsg" },
-				{ "\nPress any key to exit..." },
-			}, true, {})
-			vim.fn.getchar()
-			os.exit(1)
-		end
-	end
-	vim.opt.rtp:prepend(lazypath)
+  -- otherwise we have to bootstrap lazy ourselves
+  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+  if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+      vim.api.nvim_echo({
+        { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+        { out, "WarningMsg" },
+        { "\nPress any key to exit..." },
+      }, true, {})
+      vim.fn.getchar()
+      os.exit(1)
+    end
+  end
+  vim.opt.rtp:prepend(lazypath)
 
-	require("lazy").setup({
-		spec = {
-			-- add LazyVim and import its plugins
-			{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-			-- import/override with your plugins
-			{ import = "plugins" },
-		},
-		defaults = {
-			-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-			-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-			lazy = false,
-			-- It's recommended to leavae version=false for now, since a lot of the plugins that support versioning
-			-- have outdated releases, which may break your Neovim install.
-			version = false, -- always use the latest git commit
-			-- version = "*", -- try installing the latest stable version for plugins that support semver
-		},
-		install = { colorscheme = { "tokyonight", "habamax" } },
-		checker = {
-			enabled = true, -- check for plugin updates periodically
-			notify = false, -- notify on update
-		}, -- automatically check for plugin updates
-	})
+  require("lazy").setup({
+    spec = {
+      -- add LazyVim and import its plugins
+      { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+      -- import/override with your plugins
+      { import = "plugins" },
+    },
+    defaults = {
+      -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+      -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+      lazy = false,
+      -- It's recommended to leavae version=false for now, since a lot of the plugins that support versioning
+      -- have outdated releases, which may break your Neovim install.
+      version = false, -- always use the latest git commit
+      -- version = "*", -- try installing the latest stable version for plugins that support semver
+    },
+    install = { colorscheme = { "tokyonight", "habamax" } },
+    checker = {
+      enabled = true, -- check for plugin updates periodically
+      notify = false, -- notify on update
+    }, -- automatically check for plugin updates
+  })
 end
--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,161 +1,161 @@
 return {
-	-- autopairs support
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		config = true,
-		-- use opts = {} for passing setup options
-	},
+  -- autopairs support
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = true,
+    -- use opts = {} for passing setup options
+  },
 
-	-- use classic view with which-key instead of helix
-	{
-		"folke/which-key.nvim",
-		opts = {
-			preset = "classic",
-		},
-	},
+  -- use classic view with which-key instead of helix
+  {
+    "folke/which-key.nvim",
+    opts = {
+      preset = "classic",
+    },
+  },
 
-	-- adjust lsps and inlay hints
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				nixd = {},
-				nil_ls = false,
-				statix = {},
-				lua_ls = {},
-			},
-			inlay_hints = { enabled = false },
-		},
-	},
+  -- adjust lsps and inlay hints
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nixd = {},
+        nil_ls = false,
+        statix = {},
+        lua_ls = {},
+      },
+      inlay_hints = { enabled = false },
+    },
+  },
 
-	-- use prettierd
-	{
-		"stevearc/conform.nvim",
-		opts = function(_, opts)
-			opts.formatters_by_ft = opts.formatters_by_ft or {}
-			local supported = {
-				"css",
-				"graphql",
-				"handlebars",
-				"html",
-				"javascript",
-				"javascriptreact",
-				"json",
-				"jsonc",
-				"less",
-				"markdown",
-				"markdown.mdx",
-				"scss",
-				"typescript",
-				"typescriptreact",
-				"vue",
-				"yaml",
-			}
-			for _, ft in ipairs(supported) do
-				opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
-				table.insert(opts.formatters_by_ft[ft], "prettierd")
-			end
+  -- use prettierd
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      local supported = {
+        "css",
+        "graphql",
+        "handlebars",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "jsonc",
+        "less",
+        "markdown",
+        "markdown.mdx",
+        "scss",
+        "typescript",
+        "typescriptreact",
+        "vue",
+        "yaml",
+      }
+      for _, ft in ipairs(supported) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "prettierd")
+      end
 
-			opts.formatters = opts.formatters or {}
-			opts.formatters.prettier = {}
-			opts.formatters.prettierd = {}
-		end,
-	},
+      opts.formatters = opts.formatters or {}
+      opts.formatters.prettier = {}
+      opts.formatters.prettierd = {}
+    end,
+  },
 
-	-- changing behavior of cmp scrolling
-	{
-		"hrsh7th/nvim-cmp",
-		opts = function(_, opts)
-			local cmp = require("cmp")
+  -- changing behavior of cmp scrolling
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      local cmp = require("cmp")
 
-			opts.mapping = cmp.mapping.preset.insert(vim.tbl_extend("force", opts.mapping, {
-				["<Down>"] = cmp.config.disable,
-				["<Up>"] = cmp.config.disable,
-				["<CR>"] = cmp.config.disable,
+      opts.mapping = cmp.mapping.preset.insert(vim.tbl_extend("force", opts.mapping, {
+        ["<Down>"] = cmp.config.disable,
+        ["<Up>"] = cmp.config.disable,
+        ["<CR>"] = cmp.config.disable,
 
-				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-			}))
-			opts.experimental.ghost_text = nil
-			-- opts.snippet = {
-			--   expand = function(args)
-			--     vim.snippet.expand(args.body)
-			--   end,
-			-- }
-		end,
-	},
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+      }))
+      opts.experimental.ghost_text = nil
+      -- opts.snippet = {
+      --   expand = function(args)
+      --     vim.snippet.expand(args.body)
+      --   end,
+      -- }
+    end,
+  },
 
-	-- commands for snippets with luasnip
-	{
-		"L3MON4D3/LuaSnip",
-		keys = {
-			-- command to expand/jump forward
-			{
-				"<c-k>",
-				function()
-					local ls = require("luasnip")
-					if ls.expand_or_jumpable() then
-						ls.expand_or_jump()
-					end
-				end,
-				silent = true,
-				desc = "Expand or jump snippet",
-				mode = { "i", "s" },
-			},
-			-- command to jump backward
-			{
-				"<c-j>",
-				function()
-					local ls = require("luasnip")
-					if ls.jumpable(-1) then
-						ls.jump(-1)
-					end
-				end,
-				silent = true,
-				desc = "Jump to prev snippet",
-				mode = { "i", "s" },
-			},
-		},
-	},
+  -- commands for snippets with luasnip
+  {
+    "L3MON4D3/LuaSnip",
+    keys = {
+      -- command to expand/jump forward
+      {
+        "<c-k>",
+        function()
+          local ls = require("luasnip")
+          if ls.expand_or_jumpable() then
+            ls.expand_or_jump()
+          end
+        end,
+        silent = true,
+        desc = "Expand or jump snippet",
+        mode = { "i", "s" },
+      },
+      -- command to jump backward
+      {
+        "<c-j>",
+        function()
+          local ls = require("luasnip")
+          if ls.jumpable(-1) then
+            ls.jump(-1)
+          end
+        end,
+        silent = true,
+        desc = "Jump to prev snippet",
+        mode = { "i", "s" },
+      },
+    },
+  },
 
-	-- opencode integration
-	{
-		"NickvanDyke/opencode.nvim",
-		dependencies = {
-			-- Recommended for `ask()` and `select()`.
-			-- Required for default `toggle()` implementation.
-			{ "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
-		},
-		config = function()
-			---@type opencode.Opts
-			vim.g.opencode_opts = {
-				-- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
-			}
+  -- opencode integration
+  {
+    "NickvanDyke/opencode.nvim",
+    dependencies = {
+      -- Recommended for `ask()` and `select()`.
+      -- Required for default `toggle()` implementation.
+      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+    },
+    config = function()
+      ---@type opencode.Opts
+      vim.g.opencode_opts = {
+        -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+      }
 
-			-- Required for `opts.auto_reload`.
-			vim.o.autoread = true
+      -- Required for `opts.auto_reload`.
+      vim.o.autoread = true
 
-			-- Recommended/example keymaps.
-			vim.keymap.set({ "n", "x", "v" }, "<leader>o", "", { desc = "+opencode" })
-			vim.keymap.set({ "n", "x" }, "<leader>o?", function()
-				require("opencode").ask("@this: ", { submit = true })
-			end, { desc = "Ask opencode" })
-			vim.keymap.set({ "n", "x" }, "<leader>ox", function()
-				require("opencode").select()
-			end, { desc = "Execute opencode action…" })
-			vim.keymap.set({ "n", "x" }, "<leader>oa", function()
-				require("opencode").prompt("@this")
-			end, { desc = "Add to opencode" })
-			vim.keymap.set("n", "<leader>oo", function()
-				require("opencode").toggle()
-			end, { desc = "Toggle opencode" })
-			vim.keymap.set("n", "<S-C-u>", function()
-				require("opencode").command("messages_half_page_up")
-			end, { desc = "opencode half page up" })
-			vim.keymap.set("n", "<S-C-d>", function()
-				require("opencode").command("messages_half_page_down")
-			end, { desc = "opencode half page down" })
-		end,
-	},
+      -- Recommended/example keymaps.
+      vim.keymap.set({ "n", "x", "v" }, "<leader>o", "", { desc = "+opencode" })
+      vim.keymap.set({ "n", "x" }, "<leader>o?", function()
+        require("opencode").ask("@this: ", { submit = true })
+      end, { desc = "Ask opencode" })
+      vim.keymap.set({ "n", "x" }, "<leader>ox", function()
+        require("opencode").select()
+      end, { desc = "Execute opencode action…" })
+      vim.keymap.set({ "n", "x" }, "<leader>oa", function()
+        require("opencode").prompt("@this")
+      end, { desc = "Add to opencode" })
+      vim.keymap.set("n", "<leader>oo", function()
+        require("opencode").toggle()
+      end, { desc = "Toggle opencode" })
+      vim.keymap.set("n", "<S-C-u>", function()
+        require("opencode").command("messages_half_page_up")
+      end, { desc = "opencode half page up" })
+      vim.keymap.set("n", "<S-C-d>", function()
+        require("opencode").command("messages_half_page_down")
+      end, { desc = "opencode half page down" })
+    end,
+  },
 }
