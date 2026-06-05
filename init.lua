52ab280ff61e34cf6905fde478d055ca1f93fd90
ben <koppe.development@gmail.com>
2026-06-05 00:15:18 -0700

    attempt to fix packadd problems

--- init.lua
+++ init.lua
@@ -1,1 +1,8 @@
+if mnw ~= nil then
+  local config_path = mnw.configDir .. "/pack/mnw/start/ben"
+  if (vim.uv or vim.loop).fs_stat(config_path) then
+    vim.cmd("packadd! ben")
+  end
+end
+
 require("config.lazy")
