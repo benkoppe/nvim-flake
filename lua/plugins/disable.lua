8d1a565d60bc5ea3c16beb9543cfe069e865ea65
ben <koppe.development@gmail.com>
2026-06-06 09:17:34 -0700

    disable plugin: yanky sync with system

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,8 +1,15 @@
 -- for disabling built-in LazyVim plugins
 return {
-  -- disable bufferline.nvim
-  { "akinsho/bufferline.nvim", enabled = false },
+	-- disable bufferline.nvim
+	{ "akinsho/bufferline.nvim", enabled = false },
 
-  -- disable noice.nvim cmdline replacement
-  { "folke/noice.nvim", enabled = false },
+	-- disable noice.nvim cmdline replacement
+	{ "folke/noice.nvim", enabled = false },
+
+	-- disable yanky sync with system
+	{ "gbprod/yanky.nvim", opts = {
+		system_clipboard = {
+			sync_with_ring = false,
+		},
+	} },
 }
