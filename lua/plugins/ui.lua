7b404132be607f9ecbf3f181986c37d4c39bfa4e
ben <koppe.development@gmail.com>
2026-08-22 13:25:22 -0700

    Configure shared editor UI

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,262 @@
+local root = require("config.root")
+
+return {
+	{
+		"mini.icons",
+		lazy = false,
+		after = function()
+			require("mini.icons").setup()
+			MiniIcons.mock_nvim_web_devicons()
+		end,
+	},
+
+	{
+		"snacks.nvim",
+		lazy = false,
+		priority = 1000,
+		after = function()
+			require("snacks").setup({
+				bigfile = {},
+				indent = {},
+				input = {},
+				notifier = {},
+				quickfile = {},
+				scope = {},
+				scroll = {
+					enabled = false,
+				},
+				statuscolumn = {
+					folds = {
+						open = false,
+						git_hl = false,
+					},
+					git = {
+						patterns = {
+							"GitSign",
+							"MiniDiffSign",
+						},
+					},
+					refresh = 50,
+				},
+				terminal = {},
+				words = {},
+			})
+
+			vim.o.statuscolumn = "%!v:lua.Snacks.statuscolumn()"
+
+			vim.keymap.set("n", "<leader>n", function()
+				Snacks.notifier.show_history()
+			end, { desc = "Notification history" })
+
+			vim.keymap.set("n", "<leader>un", function()
+				Snacks.notifier.hide()
+			end, { desc = "Dismiss notifications" })
+
+			vim.keymap.set("n", "<leader>.", function()
+				Snacks.scratch()
+			end, { desc = "Toggle scratch buffer" })
+
+			vim.keymap.set("n", "<leader>S", function()
+				Snacks.scratch.select()
+			end, { desc = "Select scratch buffer" })
+
+			vim.keymap.set("n", "<leader>gg", function()
+				Snacks.lazygit({ cwd = root.git() })
+			end, { desc = "Lazygit (root)" })
+
+			vim.keymap.set("n", "<leader>gG", function()
+				Snacks.lazygit()
+			end, { desc = "Lazygit (cwd)" })
+
+			vim.keymap.set({ "n", "x" }, "<leader>gB", function()
+				Snacks.gitbrowse()
+			end, { desc = "Open in browser" })
+
+			vim.keymap.set({ "n", "x" }, "<leader>gY", function()
+				Snacks.gitbrowse({
+					open = function(url)
+						vim.fn.setreg("+", url)
+					end,
+					notify = false,
+				})
+			end, { desc = "Copy Git URL" })
+
+			vim.keymap.set("n", "<leader>ft", function()
+				Snacks.terminal(nil, { cwd = root() })
+			end, { desc = "Terminal (root)" })
+
+			Snacks.toggle.zoom():map("<leader>wm"):map("<leader>uZ")
+			Snacks.toggle.zen():map("<leader>uz")
+		end,
+	},
+
+	{
+		"tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		after = function()
+			require("tokyonight").setup({
+				style = "moon",
+			})
+
+			vim.cmd.colorscheme("tokyonight")
+		end,
+	},
+
+	{
+		"lualine.nvim",
+		event = "DeferredUIEnter",
+		after = function()
+			require("lualine").setup({
+				options = {
+					globalstatus = true,
+					theme = "auto",
+					component_separators = {
+						left = "│",
+						right = "│",
+					},
+					section_separators = "",
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = {
+						"branch",
+					},
+					lualine_c = {
+						{
+							function()
+								return "󱉭 " .. vim.fn.fnamemodify(root(), ":t")
+							end,
+							cond = function()
+								return root() ~= vim.uv.cwd()
+							end,
+						},
+						{
+							"diagnostics",
+							symbols = {
+								error = " ",
+								warn = " ",
+								info = " ",
+								hint = " ",
+							},
+						},
+						{
+							"filetype",
+							icon_only = true,
+							separator = "",
+							padding = {
+								left = 1,
+								right = 0,
+							},
+						},
+						{
+							"filename",
+							path = 1,
+							symbols = {
+								modified = "  ",
+								readonly = "",
+								unnamed = "",
+							},
+						},
+					},
+					lualine_x = {
+						"encoding",
+						"fileformat",
+						"filetype",
+					},
+					lualine_y = {
+						"progress",
+					},
+					lualine_z = {
+						"location",
+					},
+				},
+				extensions = {
+					"fzf",
+					"neo-tree",
+				},
+			})
+		end,
+	},
+
+	{
+		"which-key.nvim",
+		event = "DeferredUIEnter",
+		after = function()
+			local which_key = require("which-key")
+
+			which_key.setup({
+				preset = "helix",
+			})
+
+			which_key.add({
+				{ "<leader><tab>", group = "tabs" },
+				{ "<leader>b", group = "buffers" },
+				{ "<leader>c", group = "code" },
+				{ "<leader>d", group = "debug" },
+				{ "<leader>f", group = "file/find" },
+				{ "<leader>g", group = "git" },
+				{ "<leader>o", group = "opencode" },
+				{ "<leader>q", group = "quit/session" },
+				{ "<leader>s", group = "search" },
+				{ "<leader>u", group = "UI" },
+				{ "<leader>w", group = "windows" },
+				{ "<leader>x", group = "diagnostics/quickfix" },
+				{ "[", group = "previous" },
+				{ "]", group = "next" },
+				{ "g", group = "goto" },
+				{ "gs", group = "surround" },
+				{ "z", group = "fold" },
+			})
+
+			vim.keymap.set("n", "<leader>?", function()
+				which_key.show({ global = false })
+			end, { desc = "Buffer-local keymaps" })
+
+			vim.keymap.set("n", "<C-w><Space>", function()
+				which_key.show({
+					keys = "<C-w>",
+					loop = true,
+				})
+			end, { desc = "Window hydra" })
+		end,
+	},
+
+	{
+		"persistence.nvim",
+		event = "BufReadPre",
+		keys = {
+			{
+				"<leader>qs",
+				function()
+					require("persistence").load()
+				end,
+				desc = "Restore session",
+			},
+			{
+				"<leader>qS",
+				function()
+					require("persistence").select()
+				end,
+				desc = "Select session",
+			},
+			{
+				"<leader>ql",
+				function()
+					require("persistence").load({ last = true })
+				end,
+				desc = "Restore last session",
+			},
+			{
+				"<leader>qd",
+				function()
+					require("persistence").stop()
+				end,
+				desc = "Stop saving session",
+			},
+		},
+		after = function()
+			require("persistence").setup()
+		end,
+	},
+}
