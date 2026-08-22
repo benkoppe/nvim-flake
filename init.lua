65a15bbd5d39615b84136a8935630cd8fb8e4393
ben <koppe.development@gmail.com>
2026-08-22 12:46:45 -0700

    Replace lazy.nvim with lze

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
-require("config.lazy")
+vim.loader.enable()
+
+vim.keymap.set("n", " ", "<Nop>", {
+	silent = true,
+	remap = false,
+})
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("lze").load("plugins")
